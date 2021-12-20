2885142a8ca2fcc714e6d56f5b0e12e2ab944a01
zimiin <kahin6105@gmail.com>
2021-12-20 23:39:49 +0900

    Initial commit

---
+++ src/components/pages/IndexPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function IndexPage() {
+  return (
+    <div>
+      INDEX
+    </div>
+  )
+}
+
+export default IndexPage
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import SignUpPage from 'components/pages/SignUpPage';
+import IndexPage from 'components/pages/IndexPage';
+import NotFoundPage from 'components/pages/NotFoundPage';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SignUpPage />}>
+          <Route path="/sign-up" element={<SignUpPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
