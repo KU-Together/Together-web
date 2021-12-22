bdfd38f06d2497ca3dae674093bc47062275dd82
qooktree <qooktree@naver.com>
2021-12-22 14:10:40 +0900

    SignUp_1

--- src/components/pages/SignUpPage.tsx
+++ src/components/pages/SignUpPage.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+
+import SignUp_call from 'components/templates/SignUp_call';
+import React from 'react';
+
 
 function SignUpPage() {
   return (
     <div>
-      
+      <SignUp_call/>
     </div>
   )
 }
