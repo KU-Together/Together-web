364fa4b7b9d4ba9c2280f63212fc6f6a97c8fc69
신지민 <kahin6105@gmail.com>
2022-02-19 01:30:20 +0900

    Add App component

--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import SignUpPage from "pages/SignUpPage";
-import IndexPage from "pages/IndexPage";
-import NotFoundPage from "pages/NotFoundPage";
-import Main from "pages/main/Main";
-import { Provider } from "react-redux";
-import { store } from "store";
-import makeServer from "server";
-
-// makeServer({ environment: 'development'})
+import App from "App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<IndexPage />}>
-            <Route path="main" element={<Main />} />
-            <Route path="sign-up" element={<SignUpPage />} />
-            <Route path="*" element={<NotFoundPage />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </Provider>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
