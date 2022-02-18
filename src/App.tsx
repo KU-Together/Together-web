f377c9bc8b0eb0829c29f6ab1836a4f66efd804f
신지민 <kahin6105@gmail.com>
2022-02-19 01:34:44 +0900

    Fetch user info

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignUpPage from "pages/SignUpPage";
 import IndexPage from "pages/IndexPage";
 import NotFoundPage from "pages/NotFoundPage";
 import Main from "pages/main/Main";
-import { Provider } from "react-redux";
-import { store } from "store";
 import makeServer from "server";
+import { useDispatch, useSelector } from "react-redux";
+import { getUser, selectUser } from "slices/userSlice";
 
 // makeServer({ environment: 'development'})
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const testUserId = "842dbd0c-0c50-4329-88ac-8c133ccc13f0";
+    dispatch(getUser(testUserId));
+  }, []);
+
   return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<IndexPage />}>
-            <Route path="main" element={<Main />} />
-            <Route path="sign-up" element={<SignUpPage />} />
-            <Route path="*" element={<NotFoundPage />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </Provider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<IndexPage />}>
+          <Route path="main" element={<Main />} />
+          <Route path="sign-up" element={<SignUpPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
