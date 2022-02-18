568edfd8ad561f1bd0974c03bb97aa78e0679f29
신지민 <kahin6105@gmail.com>
2022-02-18 21:19:35 +0900

    Use useSelector

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,12 +1,11 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchAllCards, selectCards } from "slices/cardSlice";
-import { store } from "store";
 import Style from "./Main.style";
 import KanbanCol from "components/kanbanCol/KanbanCol";
 
 function Main() {
-  const cards = selectCards(store.getState());
+  const cards = useSelector(selectCards);
   const dispatch = useDispatch();
 
   useEffect(() => {
